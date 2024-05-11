a82c3f827756c89453cf1dfc4fa7f17d6c8f639e
luis <luisjosemon@gmail.com>
2024-05-10 20:55:25 -0400

    Exception

--- src/cl/praxis/miprimerjava/bibliotecas/java_lang/Exception.java
+++ src/cl/praxis/miprimerjava/bibliotecas/java_lang/Exception.java
@@ -1,20 +1,14 @@
 package cl.praxis.miprimerjava.bibliotecas.java_lang;
 
 public class Exception extends Throwable {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
 
     public static void main(String[] args) {
         try {
             // Llamamos a un método que puede lanzar una excepción
-            dividir(10, 0);
+            dividir(10, 2);
         } catch (ArithmeticException e) {
             // Capturamos la excepción específica ArithmeticException
             System.out.println(ANSI_BLUE + "No se puede dividir por cero.");
--- src/cl/praxis/miprimerjava/bibliotecas/java_lang/Thread.java
+++ src/cl/praxis/miprimerjava/bibliotecas/java_lang/Thread.java
@@ -1,16 +1,11 @@
 package cl.praxis.miprimerjava.bibliotecas.java_lang;
 
 public class Thread {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
+
     public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         // Creamos un objeto de la clase MiHilo
         MiHilo hilo = new MiHilo();
 
@@ -21,11 +16,9 @@ public class Thread {
         System.out.println(ANSI_RED + "Hilo principal: Esperando que el hilo termine.");
 
         // Esperamos a que el hilo termine su ejecución
-        try {
+
             hilo.join();
-        } catch (InterruptedException e) {
-            System.out.println(ANSI_BLACK + "Hilo principal: El hilo ha sido interrumpido.");
-        }
+
 
         // Mostramos un mensaje después de que el hilo termina su ejecución
         System.out.println(ANSI_BLUE + "Hilo principal: El hilo ha terminado.");

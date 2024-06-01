12f78d8328d688cd676c4bd9ceaffbdd45c55549
Felipe Kessi <fkessi@udec.cl>
2024-05-31 22:43:32 -0400

    Reforzamiento Java, arreglos y listas

--- src/cl/praxis/miprimerjava/java/Arreglos.java
+++ src/cl/praxis/miprimerjava/java/Arreglos.java
@@ -1,9 +1,15 @@
 package cl.praxis.miprimerjava.java;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Arreglos {
     public static void main(String[] args) {
+
+        int[] arregloDesordenado = {1,2,30,23,45,25,-12,124,0,-4};
+        int[] arregloEnteros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] copiaEnteros;
+
 
         double[] arregloNumeros = new double[10];
         arregloNumeros[0] = 1;
@@ -34,6 +40,7 @@ public class Arreglos {
         metodoToString(arregloNumeros);
         metodoToString(arregloNumeros2);
         metodoToString(arregloTextos);
+        metodoToString(arregloDesordenado);
 
         /* ciclo for para recorrer*/
         System.out.println("ciclo For");
@@ -47,9 +54,25 @@ public class Arreglos {
             System.out.println(numero);
         }
 
+        /* Métodos para arreglos */
+        // Min y Max
+        System.out.println(metodoTitulo("Min y Max"));
+        metodoMinMax(arregloNumeros2);
+        metodoMinMax(arregloDesordenado);
+        metodoMinMax(arregloNumeros);
 
+        //Metodo Sort
+        metodoSort(arregloDesordenado);
+        System.out.println("*******************************");
+
+        metodoToString(metodoOrdenador(arregloDesordenado));
+
+        //Metodo copyOf
+
+        copiaEnteros = Arrays.copyOf(arregloEnteros, arregloEnteros.length);
+        System.out.println(Arrays.toString(copiaEnteros));
     }
-
+    /******** Metodos ToString*******/
     public static void metodoToString(double[] arregloRecibido){
         System.out.println("toString by Java Util");
         System.out.println(Arrays.toString(arregloRecibido));
@@ -60,7 +83,55 @@ public class Arreglos {
     }
     public static void metodoToString(String[] arregloRecibido){
         System.out.println("toString by Java Util");
-        System.out.println(Arrays.toString(arregloRecibido));
+        System.out.println(arregloRecibido);
+    }
+
+    public static void metodoMinMax(int[] arregloRecibido){
+        int min = arregloRecibido[0];
+        int max = arregloRecibido[0];
+
+        for(int i=0; i<arregloRecibido.length; i++){
+            if(arregloRecibido[i]>max){
+                max = arregloRecibido[i];
+            }
+            if(arregloRecibido[i]<min){
+                min = arregloRecibido[i];
+            }
+        }
+        System.out.println("El elemento mínimo: "+min);
+        System.out.println("El elemento máximo: "+max);
+    }
+
+    public static void metodoMinMax(double[] arregloRecibido){
+        double valorMinimo = arregloRecibido[0];
+        double valorMaximo = arregloRecibido[0];
+
+        for(int i=0; i<arregloRecibido.length; i++){
+           valorMaximo = Math.max(arregloRecibido[i], valorMaximo);
+           valorMinimo = Math.min(arregloRecibido[i], valorMinimo);
+         }
+        System.out.println("El elemento mínimo: "+valorMinimo);
+        System.out.println("El elemento máximo: "+valorMaximo);
+    }
+
+    public static void metodoSort(int[] arregloRecibido){
+        String impresion="";
+        impresion = Arrays.toString(arregloRecibido);
+        System.out.println("Arreglo Recibido: " + impresion);
+        Arrays.sort(arregloRecibido);
+        impresion = Arrays.toString(arregloRecibido);
+        System.out.println("Arreglo ordenado: " + impresion);
+    }
+
+    public static int[] metodoOrdenador(int[] arregloRecibido){
+        Arrays.sort(arregloRecibido);
+        return arregloRecibido;
+    }
+
+    public static String metodoTitulo(String mensajeRecibido){
+        String impresion = "Sección título: "+mensajeRecibido;
+        System.out.println("entré al método titulo");
+        return impresion;
     }
 }
 
---
+++ src/cl/praxis/miprimerjava/java/Listas.java
@@ -0,0 +1,73 @@
+package cl.praxis.miprimerjava.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+
+public class Listas {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+
+        lista.add("Hola1");
+        lista.add("Hola2");
+        lista.add("Hola3");
+        lista.add("Hola4");
+        lista.add("Hola5");
+
+        String[] prueba = {"Hola1", "Hola2", "Hola3", "Hola4"};
+        String[] nuevaPrueba = Arrays.copyOf(prueba, 3);
+
+        List<String> copia = new ArrayList<>(lista);
+        lista.set(1, "jajajaj");
+        System.out.println(lista);
+
+        System.out.println(copia);
+
+        System.out.println("----------------------------------------------------");
+        System.out.println("La lista contiene: " + lista.size() + " Elementos");
+        System.out.println("El Index de 'Hola4' es: " + lista.indexOf("Hola4"));
+        System.out.println("La lista contiene 'Hola8'? " + lista.contains("Hola8"));
+        System.out.println("El elemento 3 de la lista es: " + lista.get(3));
+        System.out.println("Los elementos de la lista son:" + lista);
+        for(String el: lista){
+            System.out.println("Elemento: " + el);
+        }
+        System.out.println("----------------------------------------------------");
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println("Elemento: " + lista.get(i));
+        }
+        System.out.println("----------------------------------------------------");
+
+        List<Integer> listaNumeros = new ArrayList<>();
+        listaNumeros.add(25);
+        listaNumeros.add(258);
+        listaNumeros.add(35);
+        listaNumeros.add(2);
+        listaNumeros.add(21);
+
+        // modificar elemento dentro de la lista
+        listaNumeros.set(3, 35);
+
+        
+        System.out.println(listaNumeros.get(3));
+
+        System.out.println("----------------------------------------------------");
+        System.out.println("La lista contiene: " + listaNumeros.size() + " Elementos");
+        System.out.println("El Index de 2 es: " + listaNumeros.indexOf(2));
+        System.out.println("La lista contiene 300? " + listaNumeros.contains(300));
+        System.out.println("El elemento 3 de la lista es: " + listaNumeros.get(3));
+        System.out.println("Los elementos de la lista son:" + listaNumeros);
+
+        for(int el: listaNumeros){
+            System.out.println("Elemento: " + el);
+        }
+        System.out.println("----------------------------------------------------");
+        for (int i = 0; i < listaNumeros.size(); i++) {
+            System.out.println("Elemento: " + listaNumeros.get(i));
+        }
+        System.out.println("----------------------------------------------------");
+
+    }
+
+}
